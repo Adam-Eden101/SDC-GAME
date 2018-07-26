a778d88a7500d2bae7759c212c3fa8ce45ed29b3
Cameron-Carra <carra_c@etna-alternance.net>
2018-07-26 10:40:14 +0200

    commit letter generation

--- Assets/Scripts/GameGenerator.cs
+++ Assets/Scripts/GameGenerator.cs
@@ -3,29 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameGenerator : MonoBehaviour {
-    public int          nb_letters;
-    public GameObject[] letters;
+    public int                  nb_letters;
+    private GameObject[]        letters;
+    private GameObject[]        dismissed;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-        letters = GameObject.FindGameObjectsWithTag("Letter");
-        foreach (GameObject item in letters)
-        {
-            Debug.Log(item.name);
-        }
-        reshuffle(letters);
-        foreach (GameObject item in letters)
-        {
-            Debug.Log(item.name); 
-        }
-
+        dismissed = GameObject.FindGameObjectsWithTag("Dismissed");
+        InvokeRepeating("_main", 1.0f, 1.0f);
     }
 	
 	// Update is called once per frame
 	void Update () {
+    }
+    
+    void _main()
+    {
+        reinit();
+        if (nb_letters < 5)
+            nb_letters = 5;
+        initLetters();
+        foreach (GameObject item in (GameObject.FindGameObjectsWithTag("Letter")))
+        {
+            //Debug.Log(item.name);
+        }
+    }
+
+    void reinit()
+    {
+        Debug.Log(dismissed.Length);
+        foreach (GameObject item in dismissed)
+        {
+            Debug.Log(item.name);
+            item.tag = "Letter";
+            item.SetActive(true);
+        }
 
     }
+
+    void initLetters()
+    {
+        int i = 0;
+
+        letters = GameObject.FindGameObjectsWithTag("Letter");
+        reshuffle(letters);
+
+        foreach (GameObject item in letters)
+        {
+            if (i >= nb_letters)
+            {
+                item.tag = "Dismissed";
+            }
+            i++;
+        }
+
+        dismissed = GameObject.FindGameObjectsWithTag("Dismissed");
+        foreach (GameObject item in dismissed)
+            item.SetActive(false);
+    }
+
 
     void reshuffle(GameObject[] array)
     {
