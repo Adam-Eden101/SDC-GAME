8adc8862dac37735487439cfcb18b6b42f281535
Cameron-Carra <carra_c@etna-alternance.net>
2018-08-02 10:08:14 +0200

    Title Screen

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenController : MonoBehaviour {
+    GameObject[] flippeds;
+	// Use this for initialization
+	void Start () {
+        flippeds = GameObject.FindGameObjectsWithTag("Flip");
+        InvokeRepeating("Flip", 0, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Flip()
+    {
+        foreach (GameObject flipped in flippeds)
+            flipped.GetComponent<SpriteRenderer>().flipX = !flipped.GetComponent<SpriteRenderer>().flipX;
+    }
+
+}
